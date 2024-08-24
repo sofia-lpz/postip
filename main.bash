bf2c5d70f71a714fdf1f4bc3f0597ed84d056c32
sofia-lpz <a01028251@tec.mx>
2024-08-23 22:43:30 -0600

    added main code

---
+++ main.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+get_ip_address() {
+   hostname -I | grep -o '^[^ ]*'
+}
+
+endpoint_url="Here goes your url"
+
+ip_address=$(get_ip_address)
+
+current_date=$(date +"%Y-%m-%d %H:%M:%S")
+
+json_name="ip_info"
+
+curl -X PUT -H "Content-Type: application/json" -d "{\"ip\": \"$ip_address\", \"date\": \"$current_date\"}" $endpoint_url
