a9b0a9d347207e349a2937157b85735ee1e3e690
Sofia Moreno <a01028251@tec.mx>
2024-09-30 12:18:20 -0600

    ????

--- main.bash
+++ main.bash
@@ -4,12 +4,13 @@ get_ip_address() {
    hostname -I | grep -o '^[^ ]*'
 }
 
-endpoint_url="Here goes your url"
+endpoint_url=''
 
+echo $endpoint_url
 ip_address=$(get_ip_address)
 
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
 
 json_name="ip_info"
 
-curl -X PUT -H "Content-Type: application/json" -d "{\"ip\": \"$ip_address\", \"date\": \"$current_date\"}" $endpoint_url
+curl -X PUT -H "Content-Type: application/json" -d "{\"ip\": \"$ip_address\", \"date\": \"$current_date\"}" $endpoint_url
